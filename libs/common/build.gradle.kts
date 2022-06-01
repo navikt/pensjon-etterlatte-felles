026b1ddfbfea7b9de4c879ac7a29eac5e4ddf52e
NN <nicolas.nordhagen@nav.no>
2022-06-01 15:29:47 +0200

    forsøke release av artifact

--- libs/common/build.gradle.kts
+++ libs/common/build.gradle.kts
@@ -2,11 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    id("maven-publish")
 }
 
 repositories {
     mavenCentral()
 }
+
+group = "com.github.navikt.etterlatte"
 
 dependencies {
     api(kotlin("stdlib"))
@@ -36,5 +39,47 @@ tasks {
 
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "16"
+    }
+}
+
+val githubUser: String? by project
+val githubPassword: String? by project
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/pensjon-etterlatte-felles")
+            credentials {
+                username = githubUser
+                password = githubPassword
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+
+            pom {
+                groupId = "com.github.navikt.etterlatte"
+                artifactId = "etterlatte-common"
+
+                name.set("pensjon-etterlatte-felles")
+                description.set("Pensjon Etterlatte Felles")
+                url.set("https://github.com/navikt/pensjon-etterlatte-felles")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/pensjon-etterlatte-felles.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/pensjon-etterlatte-felles.git")
+                    url.set("https://github.com/navikt/pensjon-etterlatte-felles")
+                }
+            }
+            from(components["java"])
+        }
     }
 }
