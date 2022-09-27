e01a96f8019fcac049a584600581eff14d53d7d5
Emil Schrøder <emil.schroder@nav.no>
2022-09-27 15:17:19 +0200

    etterlatte-proxy: string json

--- apps/etterlatte-proxy/src/main/kotlin/routes/regoppslag.kt
+++ apps/etterlatte-proxy/src/main/kotlin/routes/regoppslag.kt
@@ -1,13 +1,20 @@
 package no.nav.etterlatte.routes
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.ktor.application.call
 import io.ktor.client.features.ResponseException
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
+import io.ktor.http.content.TextContent
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -35,7 +42,7 @@ fun Route.regoppslag(config: Config, stsClient: StsClient) {
                 val response = httpClient.post<HttpResponse>(regoppslagUrl + "/postadresse") {
                     header(HttpHeaders.Authorization, "Bearer $stsToken")
                     header("Nav_Callid", "barnepensjon")
-                    body = AdresseRequest("11057523044")
+                    body = TextContent(objectMapper.writeValueAsString(AdresseRequest(id)), ContentType.Application.Json)
                     pipeRequest(call)
                 }
                 call.pipeResponse(response)
@@ -54,3 +61,12 @@ data class AdresseRequest(
     val ident: String,
     val tema: String = "PEN"
 )
+
+val objectMapper: ObjectMapper = JsonMapper.builder()
+    .addModule(KotlinModule())
+    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+    .disable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS)
+    .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+    .enable(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS)
+    .build()
