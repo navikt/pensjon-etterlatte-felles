19964fe2e0606adc8b74d8442f71d029dd1b8f61
Nicolas N <nicolas.nordhagen@nav.no>
2022-10-13 09:06:17 +0200

    Fjerne siste rest av 'post-til-kafka'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,5 @@ rootProject.name = "pensjon-etterlatte-felles"
 
 include(
     "apps:etterlatte-proxy",
-    "apps:etterlatte-notifikasjoner",
-    "jobs:post-til-kafka"
+    "apps:etterlatte-notifikasjoner"
 )
