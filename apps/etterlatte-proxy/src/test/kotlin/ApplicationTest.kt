338cb8d5dfb77ad0c77735e8626c350147daed34
Sebastian Oliver Søberg <sebastian.soberg@nav.no>
2023-05-19 16:28:31 +0200

    EY-2153 Moderniser testingen

--- apps/etterlatte-proxy/src/test/kotlin/ApplicationTest.kt
+++ apps/etterlatte-proxy/src/test/kotlin/ApplicationTest.kt
@@ -2,54 +2,76 @@ package no.nav.etterlatte
 
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
-import io.ktor.http.HttpMethod
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.config.HoconApplicationConfig
-import io.ktor.server.testing.TestApplicationEngine
-import io.ktor.server.testing.createTestEnvironment
-import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.testApplication
 import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class ApplicationTest {
 
-    val mockOAuth2 = MockOAuth2Server().apply { start() }
-    var engine = TestApplicationEngine(createTestEnvironment {
-        config = HoconApplicationConfig(ConfigFactory.load()
+    val mockOAuth2 = MockOAuth2Server()
+    private lateinit var hoconApplicationConfig: HoconApplicationConfig
+
+    @BeforeAll
+    fun beforeAll() {
+        mockOAuth2.start()
+        hoconApplicationConfig = HoconApplicationConfig(ConfigFactory.load()
             .withValue("tokenx.wellKnownUrl", ConfigValueFactory.fromAnyRef(mockOAuth2.wellKnownUrl("tokenx").toString()))
             .withValue("aad.wellKnownUrl", ConfigValueFactory.fromAnyRef(mockOAuth2.wellKnownUrl("aad").toString()))
         )
-    }) {}.apply { start() }
 
+    }
     @AfterAll
     fun tearDown(){
         mockOAuth2.shutdown()
-        engine.stop(0,0)
     }
 
     @Test
     fun testRoot() {
-            engine.handleRequest(HttpMethod.Get, "internal/is_alive").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Alive", response.content)
+        testApplication {
+            environment {
+                config = hoconApplicationConfig
             }
+
+            client.get("internal/is_alive").also {
+                assertEquals(HttpStatusCode.OK, it.status)
+                assertEquals("Alive", it.body<String>())
+            }
+        }
     }
 
     @Test
     fun testAareg() {
-        engine.handleRequest(HttpMethod.Get, "aad/aareg/arbeidstaker/arbeidsforhold").apply {
-                assertEquals(HttpStatusCode.Unauthorized, response.status())
+        testApplication {
+            environment {
+                config = hoconApplicationConfig
+            }
+
+            client.get("aad/aareg/arbeidstaker/arbeidsforhold").also {
+                assertEquals(HttpStatusCode.Unauthorized, it.status)
             }
         }
+    }
 
     @Test
     fun testInntektskomponenten() {
-        engine.handleRequest(HttpMethod.Post, "aad/inntektskomponenten").apply {
-                assertEquals(HttpStatusCode.Unauthorized, response.status())
+        testApplication {
+            environment {
+                config = hoconApplicationConfig
+            }
+
+            client.post("aad/inntektskomponenten").also {
+                assertEquals(HttpStatusCode.Unauthorized, it.status)
             }
         }
+    }
 }
