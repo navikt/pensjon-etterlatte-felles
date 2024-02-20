55eab97f0ea2a60071b8cb8c632fd600cf374f7c
Mads Opheim <mads.opheim@nav.no>
2024-02-20 12:58:28 +0100

    Bruker nyaste commons-compress også her

--- apps/etterlatte-notifikasjoner/build.gradle.kts
+++ apps/etterlatte-notifikasjoner/build.gradle.kts
@@ -9,7 +9,10 @@ dependencies {
     implementation(libs.etterlatte.common)
     implementation(libs.etterlatte.ktorClientAuth)
 
-    implementation(libs.brukernotifikasjonSchemas)
+    implementation(libs.brukernotifikasjonSchemas) {
+        exclude("org.apache.commons", "commons-compress")
+    }
+    implementation(libs.commons.compress)
     implementation(libs.kafka.avro.serializer) {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
