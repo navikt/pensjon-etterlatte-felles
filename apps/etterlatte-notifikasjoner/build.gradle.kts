616338a9b4cc01943e1d22252f9c8ae75a30586f
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2024-02-20 17:59:54 +0100

    Oppgraderer kafka-manager og commons-compress (#206)
    * Ny versjon av kakfa-manager utan commons-compress-sårbarheiter
    
    * Vi bruker ikkje lenger kafkaContainer til noko her, så ryddar han like godt bort
    
    * Bruker nyaste commons-compress også her

--- apps/etterlatte-notifikasjoner/build.gradle.kts
+++ apps/etterlatte-notifikasjoner/build.gradle.kts
@@ -9,13 +9,15 @@ dependencies {
     implementation(libs.etterlatte.common)
     implementation(libs.etterlatte.ktorClientAuth)
 
-    implementation(libs.brukernotifikasjonSchemas)
+    implementation(libs.brukernotifikasjonSchemas) {
+        exclude("org.apache.commons", "commons-compress")
+    }
+    implementation(libs.commons.compress)
     implementation(libs.kafka.avro.serializer) {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
     implementation(libs.kafka.clients)
 
-    testImplementation(libs.kafka.testcontainer)
     testImplementation(libs.mockk)
     testImplementation(libs.etterlatte.commonTest)
 }
