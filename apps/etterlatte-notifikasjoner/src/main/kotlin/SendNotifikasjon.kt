0a323766f1f02e599dc51ad3e9c20a68cc2c8512
Nicolas N <Watercolours@users.noreply.github.com>
2024-02-28 09:06:30 +0100

    Fjerne avhengighet til libs (#215)

--- apps/etterlatte-notifikasjoner/src/main/kotlin/SendNotifikasjon.kt
+++ apps/etterlatte-notifikasjoner/src/main/kotlin/SendNotifikasjon.kt
@@ -1,11 +1,10 @@
 package no.nav.etterlatte
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.brukernotifikasjon.schemas.builders.BeskjedInputBuilder
 import no.nav.brukernotifikasjon.schemas.builders.NokkelInputBuilder
 import no.nav.brukernotifikasjon.schemas.input.BeskjedInput
 import no.nav.brukernotifikasjon.schemas.input.NokkelInput
-import no.nav.etterlatte.libs.common.innsendtsoeknad.common.InnsendtSoeknad
-import no.nav.etterlatte.libs.common.innsendtsoeknad.common.SoeknadType
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -29,19 +28,19 @@ class SendNotifikasjon(
     private val sikkerhetsNivaa = 4
     private val eksternVarsling = false
 
-    fun sendMessage(soeknad: InnsendtSoeknad) {
-        val innsender = soeknad.innsender.foedselsnummer.svar.value
+    fun sendMessage(soeknad: Soeknad) {
+        val innsender = soeknad.innsender.foedselsnummer.value
 
-        send(opprettNokkel(innsender), opprettBeskjed(soeknad))
+        send(opprettNokkel(innsender), opprettBeskjed(soeknad.type))
     }
 
-    internal fun opprettBeskjed(soeknad: InnsendtSoeknad): BeskjedInput {
+    internal fun opprettBeskjed(type: Soeknad.Type): BeskjedInput {
         val now = LocalDateTime.now(ZoneOffset.UTC)
         val weekFromNow = now.plusDays(7)
-        val tekst = when (soeknad.type) {
-            SoeknadType.GJENLEVENDEPENSJON -> "Vi har mottatt søknaden din om gjenlevendepensjon"
-            SoeknadType.BARNEPENSJON -> "Vi har mottatt søknaden din om barnepensjon"
-            SoeknadType.OMSTILLINGSSTOENAD -> "Vi har mottatt søknaden din om omstillingsstønad"
+        val tekst = when (type) {
+            Soeknad.Type.GJENLEVENDEPENSJON -> "Vi har mottatt søknaden din om gjenlevendepensjon"
+            Soeknad.Type.BARNEPENSJON -> "Vi har mottatt søknaden din om barnepensjon"
+            Soeknad.Type.OMSTILLINGSSTOENAD -> "Vi har mottatt søknaden din om omstillingsstønad"
         }
 
         return BeskjedInputBuilder()
@@ -65,7 +64,7 @@ class SendNotifikasjon(
         producer.send(ProducerRecord(brukernotifikasjontopic, nokkel, beskjed)).get(10, TimeUnit.SECONDS)
     } catch (e: Exception) {
         logger.error(
-            "Beskjed til $brukernotifikasjontopic (Ditt NAV) for søknad med id ${nokkel.getGrupperingsId()} feilet.",
+            "Beskjed til $brukernotifikasjontopic (Ditt NAV) for søknad med id ${nokkel.grupperingsId} feilet.",
             e
         )
     }
