e7569afe345258cdb432024a3ea43606d1969584
ingleiv Johansen <ingleiv.halvard.johansen@nav.no>
2024-04-12 10:12:44 +0200

    EY-3389 srvbarnepensjon -> srvetterlatte

--- apps/etterlatte-proxy/import-vault-token.sh
+++ apps/etterlatte-proxy/import-vault-token.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if test -f /secrets/srvbarnepensjon/username;
+if test -f /secrets/srvetterlatte/username;
 then
-    export SERVICEUSER_USERNAME=$(cat /secrets/srvbarnepensjon/username)
+    export SERVICEUSER_USERNAME=$(cat /secrets/srvetterlatte/username)
 fi
 
-if test -f /secrets/srvbarnepensjon/password;
+if test -f /secrets/srvetterlatte/password;
 then
-    export SERVICEUSER_PASSWORD=$(cat /secrets/srvbarnepensjon/password)
+    export SERVICEUSER_PASSWORD=$(cat /secrets/srvetterlatte/password)
 fi
