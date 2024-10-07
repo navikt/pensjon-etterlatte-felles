2a0a5a9653805d4588686f8961bd0bdc34f0f13a
Emil Schrøder <emil.schroder@nav.no>
2024-10-07 13:59:05 +0200

    Fjern gjenlevendepensjon fra pdfgen og notifikasjoner da det ikke er i bruk (#343)

--- apps/etterlatte-notifikasjoner/src/test/kotlin/NotifikasjonTest.kt
+++ apps/etterlatte-notifikasjoner/src/test/kotlin/NotifikasjonTest.kt
@@ -36,50 +36,6 @@ internal class NotifikasjonTest {
     }
 
     @Test
-    fun `Skal opprette notifikasjon til innsender ved innsending av gjenlevendepensjon`() {
-        val json = this::class.java.getResource("gjenlevendepensjon.json")!!.readText()
-        val soeknad: Soeknad = mapper.readValue(json)
-
-        val inspector =
-            TestRapid()
-                .apply {
-                    Notifikasjon(
-                        sendMelding,
-                        this
-                    )
-                }.apply {
-                    sendTestMessage(
-                        JsonMessage
-                            .newMessage(
-                                mapOf(
-                                    "@event_name" to "soeknad_innsendt",
-                                    "@dokarkivRetur" to "123456",
-                                    "@fnr_soeker" to "07106123912",
-                                    "@skjema_info" to mapper.readTree(json),
-                                    "@lagret_soeknad_id" to "4",
-                                    "@dokarkivRetur" to (mapOf("journalpostId" to "3"))
-                                )
-                            ).toJson()
-                    )
-                }.inspektør
-
-        assertEquals("notifikasjon_sendt", inspector.message(0).get("@event_name").asText())
-        assertEquals("Notifikasjon sendt", inspector.message(0).get("@notifikasjon").asText())
-        assertEquals("3", inspector.message(0).get("@journalpostId").asText())
-        assertEquals("4", inspector.message(0).get("@lagret_soeknad_id").asText())
-        assertEquals("SendNotifikasjon 3", inspector.key(0))
-        assertEquals(mockKafkaProducer.history().size, 1)
-        assertEquals(
-            mockKafkaProducer.history()[0].value().tekst,
-            "Vi har mottatt søknaden din om gjenlevendepensjon"
-        )
-        assertEquals(
-            soeknad.innsender.foedselsnummer.value,
-            mockKafkaProducer.history()[0].key().fodselsnummer
-        )
-    }
-
-    @Test
     fun `Skal opprette notifikasjon til innsender ved innsending av barnepensjon`() {
         val json = this::class.java.getResource("barnepensjon.json")!!.readText()
         val soeknad: Soeknad = mapper.readValue(json)
