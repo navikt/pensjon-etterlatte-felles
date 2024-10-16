dcb6f835aeffe9d0965c0429173741da37ef2cbf
Bjorn Golberg <bjorn.golberg@gmail.com>
2024-10-16 11:54:31 +0200

    Oppdaterer Inntektsjustering med nye felter

--- common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
+++ common/src/main/kotlin/no/nav/etterlatte/libs/common/inntektsjustering/Inntektsjustering.kt
@@ -1,14 +1,19 @@
 package no.nav.etterlatte.libs.common.inntektsjustering
 
 import java.time.Instant
+import java.time.LocalDate
 import java.util.UUID
 
 data class Inntektsjustering(
     val id: UUID,
+    val fnr: String,
     val inntektsaar: Int,
     val arbeidsinntekt: Int,
     val naeringsinntekt: Int,
-    val arbeidsinntektUtland: Int,
-    val naeringsinntektUtland: Int,
-    val tidspunkt: Instant,
+    val inntektFraUtland: Int,
+    val AFPInntekt: Int,
+    val AFPTjenesteordning: String?,
+    val skalGaaAvMedAlderspensjon: String,
+    val datoForAaGaaAvMedAlderspensjon: LocalDate,
+    val tidspunkt: Instant
 )
