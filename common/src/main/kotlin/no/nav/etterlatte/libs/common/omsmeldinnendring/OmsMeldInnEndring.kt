0f65bc69c73f5cf54e47169506cd0d2533ce66c6
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-10-10 14:45:50 +0200

    Oppdater meld inn endring objekt

--- common/src/main/kotlin/no/nav/etterlatte/libs/common/omsmeldinnendring/OmsMeldInnEndring.kt
+++ common/src/main/kotlin/no/nav/etterlatte/libs/common/omsmeldinnendring/OmsMeldInnEndring.kt
@@ -2,6 +2,7 @@ package no.nav.etterlatte.libs.common.omsmeldinnendring
 
 import no.nav.etterlatte.libs.common.person.Foedselsnummer
 import java.time.Instant
+import java.time.LocalDate
 import java.time.temporal.ChronoUnit
 import java.util.*
 
@@ -14,14 +15,27 @@ data class OmsMeldtInnEndring(
     val id: UUID = UUID.randomUUID(),
     val fnr: Foedselsnummer,
     val endring: OmsEndring,
+    val forventetInntektTilNesteAar: ForventetInntektTilNesteAar?,
     val beskrivelse: String,
     val tidspunkt: Instant = Instant.now().truncatedTo(ChronoUnit.SECONDS),
+)
+
+data class ForventetInntektTilNesteAar(
+    val inntektsaar: Int,
+    val arbeidsinntekt: Int,
+    val naeringsinntekt: Int,
+    val inntektFraUtland: Int,
+    val afpInntekt: Int?,
+    val afpTjenesteordning: String?,
+    val skalGaaAvMedAlderspensjon: String?,
+    val datoForAaGaaAvMedAlderspensjon: LocalDate?
 )
 
 enum class OmsEndring {
     INNTEKT,
     AKTIVITET_OG_INNTEKT,
     SVAR_PAA_ETTEROPPGJOER,
+    FORVENTET_INNTEKT_TIL_NESTE_AAR,
     ANNET,
 }
 
