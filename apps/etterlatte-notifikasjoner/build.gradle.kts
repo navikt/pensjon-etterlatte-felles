93cdc72d49a526d6e1dfab0f660a31d35c12d197
Emil Elton Nilsen <emil.elton.nilsen@nav.no>
2025-11-21 08:59:19 +0100

    Fjern unødvendig dependency

--- apps/etterlatte-notifikasjoner/build.gradle.kts
+++ apps/etterlatte-notifikasjoner/build.gradle.kts
@@ -11,14 +11,11 @@ dependencies {
         exclude("org.apache.commons", "commons-compress")
     }
 
-    implementation(libs.jacksonDatatypejsr310)
-
     implementation(libs.commons.compress)
     implementation(libs.kafka.avro.serializer) {
         exclude(group = "org.slf4j", module = "slf4j-log4j12")
     }
 
-    testImplementation(libs.jacksonDatatypejsr310)
     testImplementation(libs.mockk)
     testRuntimeOnly(libs.junit.platform.launcher)
     testImplementation(libs.test.navfelles.rapidsandriversktor)
