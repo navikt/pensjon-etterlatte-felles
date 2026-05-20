3f02178ecc74c0359992eeb9b5485d7d713575cb
Trond Valen <trond.valen@nav.no>
2026-05-20 10:39:27 +0200

    Hoppe over problematisk søknad, midlertidig.

--- apps/etterlatte-notifikasjoner/src/main/kotlin/Notifikasjon.kt
+++ apps/etterlatte-notifikasjoner/src/main/kotlin/Notifikasjon.kt
@@ -10,6 +10,7 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.math.log
 
 class Notifikasjon(
     private val sendNotifikasjon: SendNotifikasjon,
@@ -39,14 +40,20 @@ class Notifikasjon(
         runBlocking {
             val soeknad = mapper.readValue<Soeknad>(packet["@skjema_info"].toString())
 
-            sendNotifikasjon.sendMessage(soeknad)
+            val soeknadId = packet["@lagret_soeknad_id"]
+            if (soeknadId.textValue() != "27961") {
+                sendNotifikasjon.sendMessage(soeknad)
+            }
+            else {
+                logger.warn("Hoppet over utsending av notifikasjon for søknad $soeknadId")
+            }
 
             val journalpostId = packet["@dokarkivRetur"]["journalpostId"]
             JsonMessage
                 .newMessage(
                     mapOf(
                         "@event_name" to "notifikasjon_sendt",
-                        "@lagret_soeknad_id" to packet["@lagret_soeknad_id"],
+                        "@lagret_soeknad_id" to soeknadId,
                         "@journalpostId" to journalpostId,
                         "@notifikasjon" to "Notifikasjon sendt"
                     )
